27aaddf85ad5385e3b70d40342f5f489d43f0179
Jack <jacksch83@hotmail.fr>
2024-10-15 07:55:53 -0400

    Update zoomable.cpp

--- src/tiled/zoomable.cpp
+++ src/tiled/zoomable.cpp
@@ -44,6 +44,8 @@ Zoomable::Zoomable(QObject *parent)
     , mComboValidator(nullptr)
 {
     mZoomFactors = QVector<qreal> {
+        0.00390625,
+        0.0078125,
         0.015625,
         0.03125,
         0.0625,
